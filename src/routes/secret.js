ceb2ed68d67d82590f6e3c9cec1c3154d1041cc0
mattpding <mateo_1999_7@outlook.es>
2025-05-08 12:30:00 -0500

    changes

--- src/routes/secret.js
+++ src/routes/secret.js
@@ -5,5 +5,8 @@ const {auth} = require('../middlewares/authMiddleware');
 
 const router = Router();
 router.get('/secret', auth, getSecret);
+router.get('/test', async (req, res) =>  {
+    return res.status(200).json({ msg: "api online"});
+});
  
 module.exports = router;
